a983574291588ccd7151b475d09840a04d7bc14b
aimanshahbad1999 <aiman.shahbad@cuelogic.com>
2021-08-11 18:27:37 +0530

    first commit

--- vars/sayAiman.groovy
+++ vars/sayAiman.groovy
@@ -1,5 +1,50 @@
 #!/usr/bin/env groovy
 
-def call() {
-    echo "Aiman Shahbad..."
+def call(String name = 'Student') {
+    echo "Hello, ${name}."
+	if(name=='Student'){
+		pipeline{
+			agent any
+			stages{
+				stage("learn"){	
+					steps{
+						echo "Learn any technology"
+					}
+				}
+				stage("Practice"){	
+					steps{
+						echo "Practice to explore more"
+					}
+				}
+				stage("Apply"){	
+					steps{
+						echo "Build Some Project "
+					}
+				}
+			}
+		}
+	}
+	else{
+		pipeline{
+			agent any
+			stages{
+				stage("1"){	
+					steps{
+						echo "Do whatever you want "
+					}
+				}
+				stage("2"){	
+					steps{
+						echo "Do whatever you want"
+					}
+				}
+				stage("3"){	
+					steps{
+						echo "Do whatever you want "
+					}
+				}
+			}
+		}
+		
+	}	
 }
