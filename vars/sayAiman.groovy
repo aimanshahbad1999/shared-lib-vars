d5f94ffa1caddae680bb6d94f79bfde191db2943
aimanshahbad1999 <aiman.shahbad@cuelogic.com>
2021-08-11 19:51:43 +0530

    first commit 2

--- vars/sayAiman.groovy
+++ vars/sayAiman.groovy
@@ -2,49 +2,49 @@
 
 def call(String name = 'Student') {
     echo "Hello, ${name}."
-	if(name=='Student'){
-		pipeline{
-			agent any
-			stages{
-				stage("learn"){	
-					steps{
-						echo "Learn any technology"
-					}
-				}
-				stage("Practice"){	
-					steps{
-						echo "Practice to explore more"
-					}
-				}
-				stage("Apply"){	
-					steps{
-						echo "Build Some Project "
-					}
-				}
-			}
-		}
-	}
-	else{
-		pipeline{
-			agent any
-			stages{
-				stage("1"){	
-					steps{
-						echo "Do whatever you want "
-					}
-				}
-				stage("2"){	
-					steps{
-						echo "Do whatever you want"
-					}
-				}
-				stage("3"){	
-					steps{
-						echo "Do whatever you want "
-					}
-				}
-			}
-		}
+	// if(name=='Student'){
+	// 	pipeline{
+	// 		agent any
+	// 		stages{
+	// 			stage("learn"){	
+	// 				steps{
+	// 					echo "Learn any technology"
+	// 				}
+	// 			}
+	// 			stage("Practice"){	
+	// 				steps{
+	// 					echo "Practice to explore more"
+	// 				}
+	// 			}
+	// 			stage("Apply"){	
+	// 				steps{
+	// 					echo "Build Some Project "
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// else{
+	// 	pipeline{
+	// 		agent any
+	// 		stages{
+	// 			stage("1"){	
+	// 				steps{
+	// 					echo "Do whatever you want "
+	// 				}
+	// 			}
+	// 			stage("2"){	
+	// 				steps{
+	// 					echo "Do whatever you want"
+	// 				}
+	// 			}
+	// 			stage("3"){	
+	// 				steps{
+	// 					echo "Do whatever you want "
+	// 				}
+	// 			}
+	// 		}
+	// 	}
 		
-	}	
+	// }	
 }
