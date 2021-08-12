1dbb067b2532d75ac2f7a6449b7e7205e73c2738
aimanshahbad1999 <aiman.shahbad@cuelogic.com>
2021-08-12 12:31:01 +0530

    Changes in src folder

--- src/com/exm/sayAiman.groovy
+++ src/com/exm/sayAiman.groovy
@@ -3,27 +3,33 @@
 package com.exm
 
 public class sayAiman{
-    def student(){
-        pipeline{
-		agent any
-		stages{
-			stage("learn"){	
-				steps{
-					echo "Learn any technology"
-				}
-			}
-			stage("Practice"){	
-				steps{
-					echo "Practice to explore more"
-				}
-			}
-			stage("Apply"){	
-				steps{
-					echo "Build Some Project "
-				}
-			}
-			}
+    def student(name="Default"){
+		if(name=="Aiman"){
+			return "Hey hiii"
 		}
+		else{
+			return "Enter valid name"
+		}
+        // pipeline{
+		// agent any
+		// stages{
+		// 	stage("learn"){	
+		// 		steps{
+		// 			echo "Learn any technology"
+		// 		}
+		// 	}
+		// 	stage("Practice"){	
+		// 		steps{
+		// 			echo "Practice to explore more"
+		// 		}
+		// 	}
+		// 	stage("Apply"){	
+		// 		steps{
+		// 			echo "Build Some Project "
+		// 		}
+		// 	}
+		// 	}
+		// }
     }
     // def other(){
     //     node{
